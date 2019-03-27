35c74df93cc0aa9023794ea23f08447aeabcc46f
nour <ayari.nourelhouda@yahoo.com>
2019-03-27 20:59:14 +0100

    1-1-1

---
+++ jeudivja.js
@@ -0,0 +1,11 @@
+alert("hello To our little game");
+var x=2;
+var i=0;
+do{
+t=prompt("Type a random number");
+var y=Number(t);
+var i=i+1;
+} while (i<8 ) ;
+if (y=x) {
+    alert("YOU WON ON THE"+i+"TRY");
+}
